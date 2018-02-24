18f90dfafa7c15e84638ee147564750dca77f825
guapiADC <861393654@qq.com>
2018-02-25 00:59:57 +0800

    增加了print函数

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,13 @@ package com.company;
 
 public class Main {
 
+    public static void print(int index,Object object){
+        System.out.println(String.format("%d,%s",index,object.toString()));
+    }
+
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World!");
+        print(1,5>>2);
     }
 }
